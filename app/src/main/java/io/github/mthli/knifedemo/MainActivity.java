e872441fb7d38ee7ebe7dee5a0378972010eebef
Matthew Lee <matthewlee0725@gmail.com>
2015-12-10 21:03:43 +0800

    update

---
+++ app/src/main/java/io/github/mthli/knifedemo/MainActivity.java
@@ -0,0 +1,7 @@
+package io.github.mthli.knifedemo;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+
+}
---
+++ knife/src/main/java/io/github/mthli/knife/KnifeText.java
@@ -0,0 +1,23 @@
+package io.github.mthli.knife;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+public class KnifeText extends EditText {
+    public KnifeText(Context context) {
+        super(context);
+    }
+
+    public KnifeText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public KnifeText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public KnifeText(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
