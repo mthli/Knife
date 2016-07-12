59bcd4295e10a8ee314777a547229a51ed06b0dd
Ondrej Ruttkay <ondrej@automattic.com>
2016-07-12 10:50:49 +0200

    Test HTML comments

--- app/src/main/java/io/github/mthli/knifedemo/MainActivity.java
+++ app/src/main/java/io/github/mthli/knifedemo/MainActivity.java
@@ -27,6 +27,8 @@ public class MainActivity extends Activity {
     private static final String LINK = "<a href=\"https://github.com/mthli/Knife\">Link</a>";
     private static final String BLINK = "<blink meno=\"onko\">" +
             "<a href=\"https://github.com/mthli/Knife\">Link</a>" +
+            "<!-- THIS IS A COMMENT -->" +
+            "<br></ br>" +
             "<bubo><ul><li>asdfg</li></ul><blockquote>Quote</blockquote><i>lala</i></bubo>" +
             "<fero /><u>Underline</u></blink>";
     private static final String EXAMPLE = BOLD + ITALIT + UNDERLINE + STRIKETHROUGH + BULLET + QUOTE + LINK + BLINK;
